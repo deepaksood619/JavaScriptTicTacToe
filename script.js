fba47d50d4fc840007e454ac3e6da5f537d3fd95
Deepak Sood <deepaksood619@gmail.com>
2017-11-07 20:54:03 +0530

    MinMax Algorithm implemented

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ function gameOver(gameWon) {
 }
 
 function bestSpot() {
-    return emptySquares()[0];
+    return minimax(origBoard, aiPlayer).index;
 }
 
 function emptySquares() {
@@ -101,4 +101,57 @@ function declareWinner(who) {
     console.log(who);
     document.querySelector(".endgame").style.display = "block";
     document.querySelector(".endgame .text").innerText = who;
+}
+
+function minimax(newBoard, player) {
+    var availSpots = emptySquares(newBoard);
+
+    if(checkWin(newBoard, player)) {
+        return {score: -10};
+    } else if(checkWin(newBoard, aiPlayer)) {
+        return {score: 20};
+    } else if(availSpots.length === 0) {
+        return {score: 0};
+    }
+
+    var moves = [];
+    for (var i = 0; i < availSpots.length; i++) {
+        var move = {};
+        move.index = newBoard[availSpots[i]];
+        newBoard[availSpots[i]] = player;
+
+        if(player == aiPlayer) {
+            var result = minimax(newBoard, huPlayer);
+            move.score = result.score;
+        } else {
+            var result = minimax(newBoard, aiPlayer);
+            move.score = result.score;
+        }
+
+        newBoard[availSpots[i]] = move.index;
+
+        moves.push(move);
+    }
+
+    var bestMove;
+    if(player == aiPlayer) {
+            var bestScore = -10000;
+            for (var i = 0; i < moves.length; i++) {
+                if (moves[i].score > bestScore) {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        } else {
+            var bestScore = 10000;
+            for(var i = 0; i < moves.length; i++) {
+                if(moves[i].score < bestScore) {
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        
+        }
+        return moves[bestMove];
+    
 }
